a0c5af29eebd7795d1e9414fe7de048f867805f7
Ricard <drEAd9ght@gmail.com>
2015-11-26 17:56:49 +0100

    colleagues list improved: -> search bar is working as filter -> CSS add TODO tab

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,34 +2,30 @@ angular.module('mAppControllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('ColleaguesCtrl', function($scope, Backand, $http/*, mAppBackend*/) {
+.controller('ColleaguesCtrl', function($scope, Backand, $http, $ionicScrollDelegate/*, mAppBackend*/) {
 
   // the controller page is active: we must check updated data?
   //$scope.$on('$ionicView.enter', function(e) {});
 
+  /////////// COLLEAGUES DATA ////////////
+  var colleagues = [];
+
   $http({
     method: 'GET',
-    url: Backand.getApiUrl() + '/1/objects/colleagues',
-    params: {
-      pageSize: 20,
-      pageNumber: 1
-    }
-  }).success(function(response,status){
-    console.log("response id: " + response.data[1].id + " response status: " + status);
-    console.log(response.data);
-    $scope.colleagues = response.data;
-  });
-  console.log($scope.testResults);
+    url: Backand.getApiUrl() + '/1/query/data/UnassignedColleagues'
+  }).success(function(response,status){ colleagues = response; });
 
-  $scope.showAssigned = 'true';
-  $scope.switchAssigned = function(showAssigned) {
-    // document.body.classList.remove('platform-ios');
-    // document.body.classList.remove('platform-android');
-    // document.body.classList.add('platform-' + p);
-    $scope.showAssigned = showAssigned;
-    console.log("showAssigned switched to " + showAssigned);
-  }
+  $scope.getColleagues = function() { return colleagues; };
 
+  ///////////// VIEW UTILS ////////////
+  $scope.showAssigned = true;
+  $scope.switchAssigned = function(showAssigned) { $scope.showAssigned = showAssigned; }
+
+  $scope.clearSearch = function() { $scope.search = ''; };
+
+  $scope.scrollTop = function() {
+    $ionicScrollDelegate.scrollTop();
+  };
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
@@ -43,10 +39,7 @@ angular.module('mAppControllers', [])
 })
 
 .controller('MapCtrl', function($scope, $state, $window) {
-  // $scope.dev_width = $window.innerWidth;
-  // $scope.dev_height = $window.innerHeight;
   $scope.seats4 = [0,1,2,3];
-
   $scope.anar = function (where) {
     $state.go(where);
   };
